5239c7b90067bea2aabde8d091587d8b169250f3
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-25 17:05:51 +0530

    tests

--- tests/hello-world.ts
+++ tests/hello-world.ts
@@ -5,14 +5,44 @@ const assert = require("assert");
 const { SystemProgram } = anchor.web3;
 
 describe("hello-world", () => {
+  let _baseAccount;
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.HelloWorld as Program<HelloWorld>;
 
-  it("Is initialized!", async () => {
+  it("Creates a counter", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const baseAccount = anchor.web3.Keypair.generate();
+    await program.rpc.create({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: anchor.AnchorProvider.env().wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+
+    const account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+    console.log("Count 0: ", account.count.toString());
+    assert.ok(account.count.toString() == "0");
+    _baseAccount = baseAccount;
+  });
+
+  it("Increments the counter", async () => {
+    const baseAccount = _baseAccount;
+    await program.rpc.increment({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+    console.log("Count 1: ", account.count.toString());
+    assert.ok(account.count.toString() == "1");
   });
 });
